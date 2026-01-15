a046cf1b9b6a09f767e12678eeb827398accc4b3
Enocgomez <egc0031@alu.medac.es>
2026-01-15 20:27:03 +0100

    commit de la 20.27

--- src/ActividadT12/Blackjack1.java
+++ src/ActividadT12/Blackjack1.java
@@ -9,6 +9,7 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import javax.swing.JLabel;
 import java.awt.Color;
+import java.awt.FlowLayout;
 
 public class Blackjack1 extends JFrame {
 
@@ -49,6 +50,40 @@ public class Blackjack1 extends JFrame {
 		JLabel lblNewLabel = new JLabel("BLACKJACK\r\n");
 		lblNewLabel.setBackground(Color.BLACK);
 		panel.add(lblNewLabel, BorderLayout.NORTH);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(0, 128, 0));
+		panel.add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_2 = new JPanel();
+		panel_1.add(panel_2, BorderLayout.NORTH);
+		
+		JLabel lblNewLabel_1 = new JLabel("Cartas rival");
+		panel_2.add(lblNewLabel_1);
+		
+		JPanel panel_3 = new JPanel();
+		panel_1.add(panel_3, BorderLayout.CENTER);
+		panel_3.setLayout(new GridLayout(3, 0, 0, 0));
+		
+		JPanel panel_5 = new JPanel();
+		panel_3.add(panel_5);
+		panel_5.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JPanel panel_6 = new JPanel();
+		panel_3.add(panel_6);
+		panel_6.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_7 = new JPanel();
+		panel_6.add(panel_7, BorderLayout.SOUTH);
+		panel_7.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JLabel lblNewLabel_2 = new JLabel("Tus cartas");
+		panel_7.add(lblNewLabel_2);
+		
+		JPanel panel_4 = new JPanel();
+		panel_3.add(panel_4);
+		panel_4.setLayout(new GridLayout(1, 0, 0, 0));
 
 	}
 }
